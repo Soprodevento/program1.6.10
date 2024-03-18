26e541e82d0a2b843229fa526f95af1c88f23d96
Soprodevento <Sashinka2803@gmail.com>
2024-03-19 00:22:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    System.out.println(isGmailOrOutlook("kata12@gmail.com"));
+    }
+    public static boolean isGmailOrOutlook(String email) {
+        return email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+    }
+}
